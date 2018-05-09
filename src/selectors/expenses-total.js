3c2d7ec64b378f427431514c5277db057b5742db
Kyle Desktop <kylespresser@gmail.com>
2018-05-09 13:14:28 -0400

    lecture 139 refactor with instructor code

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,9 +1,5 @@
 export default (expenses) => {
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  let total = 0;
-  if (expenses.length > 0){
-    total = expenses.map(expense => expense.amount).reduce(reducer); 
-  } 
-  return total;
+  return expenses.map((expense) => expense.amount).reduce(reducer, 0);
 };
 
--- src/tests/selectors/expenses-total.test.js
+++ src/tests/selectors/expenses-total.test.js
@@ -1,36 +1,17 @@
 import moment from 'moment';
 import selectExpensesTotal from '../../selectors/expenses-total.js';
+import expenses from '../fixtures/expenses';
 
-const expenses = [{
-  id: '1',
-  description: 'Gum',
-  note: '',
-  amount: 195,
-  createdAt: 0
-}, {
-  id: '2',
-  description: 'Rent',
-  note: '',
-  amount: 109500,
-  createdAt: moment(0).subtract(4, 'days').valueOf()
-}, {
-  id: '3',
-  description: 'Credit Card',
-  note: '',
-  amount: 4500,
-  createdAt: moment(0).add(4, 'days').valueOf()
-}];
 
 test('should return 0 if no expenses', () => {
-  const expenses = [];
-  const result = selectExpensesTotal(expenses);
+  const result = selectExpensesTotal([]);
   expect(result).toBe(0);
 });
 
 test('should correctly add up a single expense', () => {
-  const singleExpense = [];
-  singleExpense.push(expenses[0]);
-  const result = selectExpensesTotal(singleExpense);
+  // const singleExpense = [];
+  // singleExpense.push(expenses[0]);
+  const result = selectExpensesTotal([expenses[0]]);
   expect(result).toBe(195);
 });
 
