b556687ffcd3b5f2561b64d83ff9bc1510d795de
Kyle Desktop <kylespresser@gmail.com>
2018-05-10 22:20:18 -0400

    lecture 157

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -35,3 +35,12 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => ({
+
+});
